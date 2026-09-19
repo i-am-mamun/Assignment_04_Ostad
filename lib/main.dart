29a9db19c8c2fa5a3b5874f7fdd3061a67629178
Md Mamun Hossain <wmamun690@gmail.com>
2026-09-20 00:15:20 +0600

    Add Appliance abstract class with subclasses

--- lib/main.dart
+++ lib/main.dart
@@ -44,4 +44,33 @@ class Developer extends Employee {
   }
 }
 
+abstract class Appliance {
+  void turnOn();
+  void turnOff();
+}
+
+class Fan extends Appliance {
+  @override
+  void turnOn() {
+    print('Fan is now running');
+  }
+
+  @override
+  void turnOff() {
+    print('Fan is turned off');
+  }
+}
+
+class Light extends Appliance {
+  @override
+  void turnOn() {
+    print('Light is switched on');
+  }
+
+  @override
+  void turnOff() {
+    print('Light is switched off');
+  }
+}
+
 void main() {}
