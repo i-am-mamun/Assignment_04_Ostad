a96655593d506be0aab0479deaeeda25267e5537
Md Mamun Hossain <wmamun690@gmail.com>
2026-09-20 00:13:59 +0600

    Add Book class with discount logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,13 @@
+class Book {
+  String title;
+  String author;
+  double price;
+
+  Book(this.title, this.author, this.price);
+
+  double discountedPrice(double discountPercent) {
+    return price - (price * discountPercent / 100);
+  }
+}
+
 void main() {}
