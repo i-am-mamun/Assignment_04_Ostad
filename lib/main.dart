4f61fe136473ab4bef1fc968a6c22186a9c76f7f
Md Mamun Hossain <wmamun690@gmail.com>
2026-09-20 00:14:15 +0600

    Add Employee, Manager and Developer classes

--- lib/main.dart
+++ lib/main.dart
@@ -10,4 +10,38 @@ class Book {
   }
 }
 
+class Employee {
+  String name;
+  double salary;
+
+  Employee(this.name, this.salary);
+}
+
+class Manager extends Employee {
+  String department;
+
+  Manager(String name, double salary, this.department) : super(name, salary);
+
+  void displayDetails() {
+    print('Manager Details:');
+    print('Name: $name');
+    print('Salary: \$${salary}');
+    print('Department: $department');
+  }
+}
+
+class Developer extends Employee {
+  String programmingLanguage;
+
+  Developer(String name, double salary, this.programmingLanguage)
+      : super(name, salary);
+
+  void displayDetails() {
+    print('Developer Details:');
+    print('Name: $name');
+    print('Salary: \$${salary}');
+    print('Programming Language: $programmingLanguage');
+  }
+}
+
 void main() {}
